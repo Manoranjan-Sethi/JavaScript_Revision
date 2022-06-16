eaf943405c4ddf7b127447a79bcd56f7dc35d3d6
Manoranjan Sethi <84295391+Manoranjan-Sethi@users.noreply.github.com>
2022-06-16 23:40:10 +0530

    HOF

---
+++ Higher_Order_Functions/doubling_numbers.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3]
+
+let result = arr.map(function(el){
+    return el+el
+})
+console.log(result)
---
+++ Higher_Order_Functions/square_problem.js
@@ -0,0 +1,5 @@
+let arr = [4,7,9]
+
+arr.forEach((item,index)=>{
+    console.log(item*item)
+})
